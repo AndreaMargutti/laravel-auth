1fe07cdd28f444ab6b139d58a6e03d6f4ccfe1d9
AndreaMargutti <marguttiandrea30@gmail.com>
2024-11-04 16:21:35 +0100

    add projects table migration

--- database/migrations/2024_11_04_150643_create_projects_table.php
+++ database/migrations/2024_11_04_150643_create_projects_table.php
@@ -11,9 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('projects', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->string('name');
+            $table->string('members');
+            $table->text('description');
         });
     }
 
@@ -22,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('projects');
     }
 };
--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -3,6 +3,25 @@
 
 @section('content')
 
-<h1>Sono l'indice</h1>
+<h1 class="py-3">Sono l'indice</h1>
+
+<table class="table table-striped">
+    <thead>
+      <tr>
+        <th scope="col">id</th>
+        <th scope="col">Name</th>
+        <th scope="col">Members</th>
+        <th scope="col">Description</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <th></th>
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr>
+    </tbody>
+  </table>
 
 @endsection
