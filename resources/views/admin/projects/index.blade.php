ded8e1538f9cb5a5fe129664f791259c750d3dd9
AndreaMargutti <marguttiandrea30@gmail.com>
2024-11-05 14:17:38 +0100

    show completed

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -12,15 +12,24 @@
         <th scope="col">Name</th>
         <th scope="col">Members</th>
         <th scope="col">Description</th>
+        <th scope="col">Actions</th>
       </tr>
     </thead>
     <tbody>
         @foreach ($projects as $project )
         <tr>
             <th>{{$project->id}}</th>
-            <td>{{$project->name}}</td>
+            <td>
+            <a href="{{route('admin.projects.show', $project)}}">
+                {{$project->name}}
+            </a>
+            </td>
             <td>{{$project->members}}</td>
             <td>{{$project->description}}</td>
+            <td>
+                <button class="btn btn-primary">Edit</button>
+                <button class="btn btn-warning">Delete</button>
+            </td>
         </tr>
         @endforeach
     </tbody>
